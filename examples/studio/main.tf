0c4689c4b5ae15b5f209a8a89afa0535b604c0e9
Bryant Biggs <bryantbiggs@gmail.com>
2023-03-05 09:38:48 -0500

    chore: Update configurations prior to transfer to use current standards

--- examples/studio/main.tf
+++ examples/studio/main.tf
@@ -6,7 +6,7 @@ data "aws_availability_zones" "available" {}
 
 locals {
   name   = replace(basename(path.cwd), "-cluster", "")
-  region = "us-east-1"
+  region = "eu-west-1"
 
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)

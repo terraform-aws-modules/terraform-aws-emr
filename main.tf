60fc33797ab652292365d6fc9acad61f49915383
Bryant Biggs <bryantbiggs@gmail.com>
2025-03-29 17:53:03 -0500

    fix: Add dependency on service security group rules

--- main.tf
+++ main.tf
@@ -325,6 +325,7 @@ resource "aws_emr_cluster" "this" {
     aws_iam_role_policy_attachment.service_pass_role,
     aws_iam_role_policy_attachment.instance_profile,
     aws_iam_role_policy_attachment.autoscaling,
+    aws_security_group_rule.service
   ]
 
   lifecycle {
